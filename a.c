b99d6efdd7ee83b3c7cf9a2d9cfe642d7687df03
Ahmed Asfah <44223597+Asfah@users.noreply.github.com>
2019-01-09 07:26:38 +0500

    Create a.c

---
+++ a.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int n=5, i, j, k;
+
+    for(i=n-1; i>=0; i--)
+    {
+        for(j=n-1; j>i; j--)
+        {
+            printf(" ");
+        }
+
+        for(k=0; k<=i; k++)
+        {
+            printf("%c", k+65);
+        }
+
+        printf("\n");
+    }
+
+
+
+
+
+}
